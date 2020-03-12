bfc7b5639529713f07c46deca964ec3db411fb9a
serra-vp <vincent.serra@boom.camp>
2020-03-12 15:46:09 +0800

    Fixed HTML in abstract

--- Week-2/Day-3/Abstract/index.php
+++ Week-2/Day-3/Abstract/index.php
@@ -1,4 +1,4 @@
-<?php /*<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -6,32 +6,29 @@
   <title>PHP Week 2 Day 3 - Abstract</title>
 </head>
 <body>
-  
+  <?php 
+    abstract class APhpClass {
+      // Define your own methods here
+      protected $mentor;
+      public function __construct($mentor){
+        $this->mentor = $mentor;
+      }
+      abstract protected function getMentor();
+    }
+
+    class Mentor extends APhpClass {
+      // class methods
+      protected function getMentor(){
+        return $this->mentor;
+      }
+
+      public function teach(){
+        return "Mentor {$this->getMentor()} teaches PHP.";
+      }
+    }
+
+    $mentor = new Mentor("Jino Lacson");
+    echo $mentor->teach();
+  ?>  
 </body>
-</html> */ ?>
-
-<?php 
-
-abstract class APhpClass {
-  // Define your own methods here
-  protected $mentor;
-  public function __construct($mentor){
-    $this->mentor = $mentor;
-  }
-  abstract protected function getMentor();
-}
-
-class Mentor extends APhpClass {
-  // class methods
-  protected function getMentor(){
-    return $this->mentor;
-  }
-
-  public function teach(){
-    return "Mentor {$this->getMentor()} teaches PHP.";
-  }
-}
-
-$mentor = new Mentor("Jino Lacson");
-echo $mentor->teach();
-?>
+</html>
