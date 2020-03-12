b67c61e3d5d2bcc01010de936f807170f220fc22
serra-vp <vincent.serra@boom.camp>
2020-03-12 15:45:04 +0800

    Done with Interface

--- Week-2/Day-3/Abstract/index.php
+++ Week-2/Day-3/Abstract/index.php
@@ -14,11 +14,24 @@
 
 abstract class APhpClass {
   // Define your own methods here
+  protected $mentor;
+  public function __construct($mentor){
+    $this->mentor = $mentor;
+  }
+  abstract protected function getMentor();
 }
 
 class Mentor extends APhpClass {
   // class methods
+  protected function getMentor(){
+    return $this->mentor;
+  }
+
+  public function teach(){
+    return "Mentor {$this->getMentor()} teaches PHP.";
+  }
 }
 
 $mentor = new Mentor("Jino Lacson");
+echo $mentor->teach();
 ?>
